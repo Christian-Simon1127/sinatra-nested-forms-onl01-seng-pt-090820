8fa54bdf9c31f6040b187931951e955f229faa37
Christian Simon <csimon1127@gmail.com>
2020-12-16 17:43:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require './environment'
-require_relative ''
-require_relative ''
+require_relative 'models/pirate.rb'
+require_relative 'models/ship.rb'
 
 module FormsLab
   class App < Sinatra::Base
