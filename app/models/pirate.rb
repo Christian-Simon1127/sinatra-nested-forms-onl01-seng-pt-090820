c453301e6cc49cb7359324761f9666593732d720
Christian Simon <csimon1127@gmail.com>
2020-12-16 16:16:17 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -3,6 +3,12 @@ class Pirate
   
   @@all = []
   
+  def initialize(name, weight, height)
+    @name = name 
+    @weight = weight 
+    @height = height
+  end
+  
   def self.all 
     @@all
   end 
